64db7d69cc7507e7afe4fc74178195e00a588267
Cameron <csavage5@me.com>
2020-05-20 17:20:15 -0700

    Added A* search with Misplaced Tile heuristic, refactored shuffle function

--- a1.py
+++ a1.py
@@ -1,23 +1,40 @@
 #a1.py
 
 from search import *
+import time
 
-def createRandomTuple():
-    sortedArray = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    random.shuffle(sortedArray)
-    return tuple(sortedArray)
+#python passing info
+# **modifying** a passed object will act as pass by reference
+# **reassigning** a passed object will act as pass by value
+
+#--- Question 1 ---#
+# shuffle the 8-puzzle by starting with the goal
+# and making a random number of random legal moves
+def shufflePuzzle(puzzle):
+    #move a random number of times
+    numMoves = random.randint(151, 200)
+    for x in range(numMoves):
+        actions = puzzle.actions(puzzle.initial)
+        randChoice = random.randint(0, len(actions)-1)
+        newState = puzzle.result(puzzle.initial, actions[randChoice])
+        puzzle = EightPuzzle(newState)
+
+    return puzzle
 
 
 def make_rand_8puzzle():
-    #check solvability of createRandom8Tuple
-    tuple = createRandomTuple()
-    puzzle = EightPuzzle(tuple, goal = (1, 2, 3, 4, 5, 6, 7, 8, 0))
-
-    while not puzzle.check_solvability(tuple):
+    #create and shuffle 8-puzzle
+    state = (1, 2, 3, 4, 5, 6, 7, 8, 0)
+    puzzle = EightPuzzle(state)
+    puzzle = shufflePuzzle(puzzle)
+    state = puzzle.initial
+    #verify solvability of createRandom8Tuple
+    while not puzzle.check_solvability(state):
         print("Generated 8-puzzle not solvable, trying again...")
         
-        tuple = createRandomTuple()
-        puzzle = EightPuzzle(tuple, goal = (1, 2, 3, 4, 5, 6, 7, 8, 0))
+        puzzle = EightPuzzle(state)
+        puzzle = shufflePuzzle(puzzle)
+        state = puzzle.initial
         
     print("Found solvable 8-puzzle")
     return puzzle
@@ -27,10 +44,10 @@ def display(state):
     strRow = ""
     for i in range(9):
         #replace 0 with *
-        if state[i] == "0":
+        if state[i] == 0:
             strRow += "*"
         else:
-            strRow += state[i]
+            strRow += str(state[i])
 
         #check for last char in row & print
         if (i + 1) % 3 == 0:
@@ -39,5 +56,30 @@ def display(state):
         else:
             strRow += " "
 
+
+#--- Question 2 ---#
+
+#seach using Misplaced Tile heuristic
+def searchAStarMisplacedTile(state):
+    print("Solving with heuristic: Misplaced Tile")
+    puzzle = EightPuzzle(state)
+    start_time = time.time()
+    # TODO modify A* algorithm to display total number of tiles moved and total number of tiles remaining in frontier
+    nodeSolution = astar_search(puzzle, display = True)
+    elapsed_time = time.time() - start_time
+    print("Elapsed time for Misplaced Tile Heuristic: " + str(round(elapsed_time, 2)) + " seconds")
+    #display(nodeSolution.state)
+
+
+puzzle = make_rand_8puzzle()
+display(puzzle.initial)
+
+searchAStarMisplacedTile(puzzle.initial)
+
+#search using Mahattan heuristic
+#def searchAStarManhattan(puzzle):
+
+#search using the max of Manhattan and Misplaced Tile heuristic
+#def searchAStarMax(puzzle):
 
     

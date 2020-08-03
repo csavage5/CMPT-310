05121ae182b3fc8acd4d92478270fd9b6d2f0e3c
Cameron <csavage5@me.com>
2020-08-03 16:58:12 -0700

    created packages

---
+++ final-project/src/cmpt310/a5/Main.java
@@ -0,0 +1,12 @@
+package cmpt310.a5;
+
+/**
+ * Entry point for Reversi game
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //instantiate controller class(es?)
+
+    }
+}
